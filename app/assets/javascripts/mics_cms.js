f76568adef9ec75cc0a1feeea59f7cb44aa9d2af
phantomfreeJr <vdavis@siu.edu>
2016-07-04 23:40:37 -0600

    update look of site

--- app/assets/javascripts/mics_cms.js
+++ app/assets/javascripts/mics_cms.js
@@ -1,13 +1,14 @@
 /*
-openNav = ->
-  $(document).getElementById('main').style.marginLeft = '250px'
-  $(document).getElementById('mySidenav').style.width = '250px'
-  $(document).body.style.backgroundColor = 'rgba(0,0,0,0.4)'
+openNav ->
+  document.getElementById('main').style.marginLeft = '250px'
+  document.getElementById('mySidenav').style.width = '250px'
+  document.body.style.backgroundColor = 'rgba(0,0,0,0.4)'
   return
 
-closeNav = ->
-  $(document).getElementById('mySidenav').style.width = '0'
-  $(document).getElementById('main').style.marginLeft = '0'
+closeNav ->
+  document.getElementById('mySidenav').style.width = '0'
+  document.getElementById('main').style.marginLeft = '0'
+  document.body.style.backgroundColor = "#454545";
   return
 */
 
