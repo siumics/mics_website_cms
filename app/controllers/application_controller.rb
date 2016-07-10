9af1b70c2c7928ce89eb1d6984d4c5f87f83ae1d
phantomfreeJr <vdavis@siu.edu>
2016-07-09 22:11:52 -0600

    fix deivse issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,6 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :first_name << :last_name << :phone_number << :dawgtag
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :dawgtag])
   end
 end
